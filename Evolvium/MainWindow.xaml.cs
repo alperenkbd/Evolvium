bf1f14d7491dc7ca5455a7582788c04c34654265
Alperen0110 <alperen_kabadayi@hotmail.com>
2024-11-14 16:49:47 +0000

    change main window.

--- Evolvium/MainWindow.xaml.cs
+++ Evolvium/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Evolvium.Presentation;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,8 @@ namespace Evolvium
         public MainWindow()
         {
             InitializeComponent();
+
+            MainFrame.Navigate(new Login());
         }
     }
 }
