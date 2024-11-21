9d3756eb6f3aa6aa6dbacebafdb44cd9f32d38e7
Alperen0110 <alperen_kabadayi@hotmail.com>
2024-11-21 16:49:03 +0000

    Add models

---
+++ Evolvium/Models/Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Evolvium.Presentation.Models
+{
+    internal class Module
+    {
+        public string Number { get; set; }
+        public string ModuleName { get; set; }
+        public string DegreeName { get; set; }
+        public string MaxScore { get; set; }
+        public ICommand EditCommand { get; set; }
+    }
+}
---
+++ Evolvium/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolvium.Presentation.Models
+{
+    internal class Student
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Degree { get; set; }
+        public string Year { get; set; }
+    }
+}
